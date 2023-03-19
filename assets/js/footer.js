44093d1d591c9e1ab4951093848cc27026a38da9
digitalaltar <128258025+digitalaltar@users.noreply.github.com>
2023-03-19 14:04:16 -0700

    Update

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,3 +1,4 @@
+/* Footer */
 function adjustFooter() {
       let body = document.body;
       let html = document.documentElement;
@@ -15,3 +16,29 @@ function adjustFooter() {
     
 adjustFooter();
 window.addEventListener('resize', adjustFooter);
+
+/* Gallery */
+function setSameHeight() {
+  // Get all the list items in the nft list
+  const nftItems = document.querySelectorAll('.nft-list figure');
+
+  // Reset the height of all the list items
+  nftItems.forEach(item => item.style.height = 'auto');
+
+  // Find the tallest list item
+  let tallestItem = 0;
+  nftItems.forEach(item => {
+    if (item.offsetHeight > tallestItem) {
+      tallestItem = item.offsetHeight;
+    }
+  });
+
+  // Set the height of all the list items to the tallest item
+  nftItems.forEach(item => item.style.height = tallestItem + 'px');
+}
+
+// Call the function when the page loads
+setSameHeight();
+
+// Call the function when the window is resized
+window.addEventListener('resize', setSameHeight);
